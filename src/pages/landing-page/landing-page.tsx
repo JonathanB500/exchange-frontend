4097acf4c76c45845f1029f3afaa4bd5371d7693
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-10-29 12:01:40 -0500

    Add landing page

---
+++ src/pages/landing-page/landing-page.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import StockList from "../../components/stock-list/StockList";
+import './landing-page.css'
+
+type Props = {
+    img: string,
+    title: string,
+    author: string
+};
+const LandingPage = () => {
+  return (
+  <div>
+    <h1 className="Titles">Live Data Market</h1>
+    <p className="Paragraphs">Analyze currencies and stocks in real time, staying updated with the latest market trends</p>
+    <StockList props={itemDataCoins}/>
+    <StockList props={itemDataStock}/>
+  </div>)
+};
+
+export default LandingPage;
+
+
+const itemDataCoins = [
+    {
+      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      alt: 'Breakfast',
+      symbol: 'BITCOIN',
+      price: 10000290239,
+      volume: 1089238932893,
+      coinMarketCap: 13298498438923
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      alt: 'Breakfast',
+      symbol: 'DOGCOIN',
+      price: 10000290239,
+      volume: 1089238932893,
+      coinMarketCap: 13298498438923
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      alt: 'Breakfast',
+      symbol: 'JEVCOIN',
+      price: 10000290239,
+      volume: 1089238932893,
+      coinMarketCap: 13298498438923
+    },
+  ];
+
+  const itemDataStock = [
+    {
+      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      alt: 'Breakfast',
+      symbol: 'TSL',
+      price: 10000290239,
+      volume: 1089238932893,
+      coinMarketCap: 13298498438923
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      alt: 'Breakfast',
+      symbol: 'AMZ',
+      price: 10000290239,
+      volume: 1089238932893,
+      coinMarketCap: 13298498438923
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      alt: 'Breakfast',
+      symbol: 'GOOL',
+      price: 10000290239,
+      volume: 1089238932893,
+      coinMarketCap: 13298498438923
+    },
+  ];
