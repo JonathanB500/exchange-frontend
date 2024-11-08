2f42bf827e74c59a2ad2648a6e0d8458b66e9d2d
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-11-08 13:57:22 -0500

    Add market service inside of landing page

--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -2,8 +2,26 @@ import StockList from "../../components/stock-list/StockList";
 import './landing-page.css'
 import Footer from "../../components/footer/Footer";
 import Carousel from "../../components/carousel/Carousel";
+import { useEffect, useState } from "react";
+import Market from "../../services/Market";
+
 
 const LandingPage = () => {
+  const [market, setMarket] = useState({latest_stocks:[], latest_cryptos:[]})
+
+useEffect(() => {
+  async function fetchData() {
+    try{
+      const market=await Market.getAllMarket();
+      setMarket(market)
+    }
+    catch(exception){
+      console.log(exception)
+    }
+  }
+  fetchData()
+
+},[]);
 
   return (
   <div className="landingPage-Container">
@@ -19,9 +37,9 @@ const LandingPage = () => {
         <h1 className="Titles">Live Data Market</h1>
         <p className="Paragraphs">Analyze currencies and stocks in real time, staying updated with the latest market trends</p>
         <h2>Crypto</h2>
-        <StockList props={getCoins()}/>
+        <StockList props={market.latest_stocks}/>
         <h2>Stocks</h2>
-        <StockList props={getStocks()}/>
+        <StockList props={market.latest_cryptos}/>
       </div>
       
     </div>
@@ -32,67 +50,5 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-
-const getCoins = () =>{
-  console.log("Call!");
-  const itemDataCoins = [
-    {
-      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      alt: 'Breakfast',
-      symbol: 'BITCOIN',
-      price: 10000290239,
-      volume: 1089238932893,
-      coinMarketCap: 13298498438923
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      alt: 'Breakfast',
-      symbol: 'DOGCOIN',
-      price: 10000290239,
-      volume: 1089238932893,
-      coinMarketCap: 13298498438923
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      alt: 'Breakfast',
-      symbol: 'JEVCOIN',
-      price: 10000290239,
-      volume: 1089238932893,
-      coinMarketCap: 13298498438923
-    },
-  ];
-  return itemDataCoins;
-}
-
-const getStocks = () =>{
-  const itemDataStock = [
-    {
-      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      alt: 'Breakfast',
-      symbol: 'TSL',
-      price: 10000290239,
-      volume: 1089238932893,
-      coinMarketCap: 13298498438923
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      alt: 'Breakfast',
-      symbol: 'AMZ',
-      price: 10000290239,
-      volume: 1089238932893,
-      coinMarketCap: 13298498438923
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      alt: 'Breakfast',
-      symbol: 'GOOL',
-      price: 10000290239,
-      volume: 1089238932893,
-      coinMarketCap: 13298498438923
-    },
-  ];
-
-  return itemDataStock;
-}
 
 
