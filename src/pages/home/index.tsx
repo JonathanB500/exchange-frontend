ee0625aca309143ba47ce3b2367559e3315c33ac
Santiago Alvarez Ricardo <santiago.alvarez@endava.com>
2024-11-12 07:31:14 -0500

    add basic structure to log-in page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,31 @@
+import { ChevronLeftOutlined } from "@mui/icons-material";
+import { DialogTitle, Button, Drawer, IconButton} from "@mui/material";
 import React from "react";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  const [open, setOpen] = React.useState(false);
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  return <div>
+    <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+    <Drawer open={open} onClose={toggleDrawer(false)}>
+      <IconButton onClick={handleDrawerClose}>
+        <ChevronLeftOutlined/>
+      </IconButton>
+      <DialogTitle>Title</DialogTitle>
+    </Drawer>
+     
+  </div>;
+
 };
 
 export default Home;
